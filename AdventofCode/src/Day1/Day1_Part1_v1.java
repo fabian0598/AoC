47a49c61d29d47a75ddd43fdbc8abebed218c63e
Fabian Winklbauer <fabian.winklbauer@gmail.com>
2022-12-03 06:41:26 +0100

    Cleanup

--- AdventofCode/src/Day1/Day1_Part1_v1.java
+++ AdventofCode/src/Day1/Day1_Part1_v1.java
@@ -10,14 +10,12 @@ public class Day1_Part1_v1 {
 		// TODO Auto-generated method stub
 		try(BufferedReader br = new BufferedReader(new FileReader("/Users/Fabian/eclipse-workspace/FreeTimeTBD/src/aoc/input_day1.txt"))) {
 		    int pointer = 0;
-		    int maxElf = 0;
 		    int maxValue = 0;
 		    int currentValue = 0;
 			for(String line; (line = br.readLine()) != null; ) {
 		        if (line.trim().isEmpty()) {
 		        	if (currentValue > maxValue) {
 		        		maxValue = currentValue;
-		        		maxElf = pointer;
 		        	}
 		        	pointer += 1;
 		        	currentValue = 0;
@@ -28,7 +26,6 @@ public class Day1_Part1_v1 {
 		    }
 			if (currentValue > maxValue) {
         		maxValue = currentValue;
-        		maxElf = pointer;
         	}
 			System.out.println(maxValue);
 		    // line is not visible here.
