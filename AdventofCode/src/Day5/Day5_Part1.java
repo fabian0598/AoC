aec215b0c8caeb1aa75ea3acc04055a9adc1883e
Fabian Winklbauer <fabian.winklbauer@gmail.com>
2022-12-05 11:38:10 +0100

    Day5

---
+++ AdventofCode/src/Day5/Day5_Part1.java
@@ -0,0 +1,54 @@
+package Day5;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.IntStream;
+
+public class Day5_Part1 {
+
+	@SuppressWarnings({ "resource" })
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+		// Read input
+		String s = new String((new FileInputStream("src/Day5/input_day5.txt")).readAllBytes(), StandardCharsets.UTF_8);
+		String[] lineSplit = s.split("\n");
+
+		// Set up stacks
+		List<Stack<String>> containers = new ArrayList<Stack<String>>();
+		IntStream.range(0, 9).forEach(i -> containers.add(new Stack<String>()));
+		
+		for(int i = 7; i >= 0; i--) {
+			int j = 0;
+			int k = 0;
+			lineSplit[i] += " "; // Add space to make each container regular
+			while(j < lineSplit[i].length()) {	
+				String subString = lineSplit[i].substring(j, j+3);
+				j += 4;
+				if(!subString.equals("   ")) containers.get(k).add(subString);
+				k++;
+			}
+		}
+		
+		containers.forEach(System.out::println);
+		System.out.println("\n");
+		
+		for(int i = 10; i < lineSplit.length; i++) {
+			String[] split = lineSplit[i].split(" ");
+			for(int j = 0; j < Integer.valueOf(split[1]); j++) {
+				Stack<String> from = containers.get(Integer.valueOf(split[3])-1);
+				Stack<String> to = containers.get(Integer.valueOf(split[5])-1);
+				to.add(from.peek());
+				from.pop();
+			}
+		}
+		
+		containers.forEach(System.out::println);
+		containers.forEach(i -> System.out.print(i.peek().charAt(1)));
+	}
+
+}
