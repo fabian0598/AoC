47ea12a1c6b86bee485d65c245cfcd4babf36cb3
Fabian Winklbauer <fabian.winklbauer@gmail.com>
2022-12-06 08:48:06 +0100

    Fixes

--- AdventofCode/src/Day6/Day6_Part1.java
+++ AdventofCode/src/Day6/Day6_Part1.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Day6_Part1 {
 
